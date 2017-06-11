22a6cf120adbb66fa01132cd6c9c16140ddca706
parassd <parassd@gmail.com>
2017-06-11 16:43:24 +0530

    Add files via upload

---
+++ MATLAB_code_for_Xray.m
@@ -0,0 +1,15 @@
+clc;
+clear all;
+close all;
+I = imread('sample1.png');
+figure, imshow(I);
+if size(I,3)==3
+    J = rgb2gray(I);
+    figure, imshow(J);
+    I = J;
+end
+K=medfilt2(I);
+figure,imshow(K);
+H = fspecial('average', [3 3]);
+L = imfilter(I,H);
+figure, imshow(L);
